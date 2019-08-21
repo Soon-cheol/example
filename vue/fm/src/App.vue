92d10a6a3a99b9eef8573fcf1d0f0eabae4eea50
sooncheol.choi <sooncheol.choi@emotion.co.kr>
2019-08-21 09:50:22 +0900

    FM

--- vue/fm/src/App.vue
+++ vue/fm/src/App.vue
@@ -1,23 +1,69 @@
 <template>
   <div id="app">
-
+    <div class="info">
+      <ul>
+        <li>
+          <span>{{number}}.</span>
+          <span>{{name}}</span>
+        </li>
+        <li>
+          <span>Team</span>
+          <span>{{team}}</span>
+        </li>
+        <li>
+          <span>Position</span>
+          <span>{{position}}</span>
+        </li>
+        <li>
+          <span>Nation</span>
+          <span>{{nation}}</span>
+        </li>
+        <li>
+          <span>Age</span>
+          <span>{{age}}</span>
+        </li>
+        <li>
+          <span>Height</span>
+          <span>{{height}}</span>
+        </li>
+        <li>
+          <span>Foot</span>
+          <span>{{foot}}</span>
+        </li>
+      </ul>
+    </div>
+    <div class="ground position433">
+      <Player />
+      <Player />
+      <Player />
+      <Player />
+      <Player />
+      <Player />
+      <Player />
+      <Player />
+      <Player />
+      <Player />
+      <Player />
+    </div>
   </div>
 </template>
 
 <script>
+import Player from './components/player.vue'
+
 export default {
+  components: {
+    Player
+  },
   data() {
     return {
-      message: 'Hello world!',
+      
     };
   },
 };
 </script>
 
 <style>
-#app {
-  width:1000px;
-  height:600px;
-  font-size: 18px;
-}
+@import url('/src/assets/css/reset.css');
+@import url('/src/assets/css/layout.css');
 </style>
