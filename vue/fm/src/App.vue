9ce71bc4a05398c3124b749b53666a417085f901
sooncheol <sooncheolchoi7@gmail.com>
2019-08-28 20:40:23 +0900

    라우터

--- vue/fm/src/App.vue
+++ vue/fm/src/App.vue
@@ -1,27 +1,43 @@
 <template>
   <div id="app">
+    <div>
+      <router-link to="/for1"><button>fomation1</button></router-link>
+      <router-link to="/for2"><button>fomation2</button></router-link>
+    </div>
     <div class="infomation">
       <Infomation />
     </div>
     <div class="ground position433">
-      <Formation :props="data" />
+      <router-view></router-view>
     </div>
   </div>
 </template>
 
 <script>
 import data from './data'
-import Formation from './Formation.vue'
+import Formation1 from './Formation1.vue'
+import Formation2 from './Formation2.vue'
 import Infomation from './Infomation.vue'
+import vueRouter from 'vue-router'
+
+const router = new vueRouter({
+  routes: [
+    {path: '/for1', component: Formation1},
+    {path: '/for2', component: Formation2}
+  ]
+})
 
 export default {
+  name : 'app',
+  router,
   components: {
-    Formation,
+    Formation1,
+    Formation2,
     Infomation
   },
-    props: {
-        props: {}
-    },
+  props: {
+      props: {}
+  },
   data : function() {
       return {
           data : data
--- vue/fm/src/main.js
+++ vue/fm/src/main.js
@@ -1,9 +1,11 @@
 import Vue from 'vue';
 import App from './App.vue';
+import VueRouter from 'vue-router'
 
 import './assets/css/reset.css';
 import './assets/css/layout.css';
 
+Vue.use(VueRouter);
 Vue.config.productionTip = false;
 
 new Vue({
