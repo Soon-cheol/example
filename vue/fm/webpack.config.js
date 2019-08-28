3776eb6922e5e551634c0a882e7c9ead47d989c8
sooncheol.choi <sooncheol.choi@emotion.co.kr>
2019-08-28 10:43:49 +0900

    Port 변경

--- vue/fm/webpack.config.js
+++ vue/fm/webpack.config.js
@@ -11,14 +11,14 @@ module.exports = {
             { test: /\.vue$/, use: 'vue-loader' },
             {
                 test: /\.css$/,
-                use: ['style-loader', 'css-loader?sourceMap=true'],
+                use: ['style-loader', 'css-loader'],
             },
         ]
     },
     devServer: {
         open: true,
         hot: true, // 소스가 변경되면 자동으로 빌드되어 반영된다.
-        port: 9002, // 서버 포트
+        port: 9000, // 서버 포트
         // inline: true, // 핫리로드를 위한 스크립트를 삽입한다. 
         // host: '0.0.0.0', // localhost 를 사용하면 외부에서 접근할 수 없다. 
         // contentBase: './dist' // 서비스 웹 루트 경로
