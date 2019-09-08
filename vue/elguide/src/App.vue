904024d1557402d3a91d3b78a31eef5702a46adb
sooncheolchoi <sooncheolchoi7@gmail.com>
2019-09-08 13:28:08 +0900

    elguide 새로 셋

--- vue/elguide/src/App.vue
+++ vue/elguide/src/App.vue
@@ -1,65 +1,28 @@
 <template>
-        <div id="wrap">
-        <!-- header -->
-        <div id="header" v-if="config.layout">
-            <h1><img class="hanwha-logo" src="./assets/svg/hanwha_logo.svg"></h1>
-            <div id="gnb">
-              <Gnb :menu="menuInfo" />
-            </div>
-        </div>
-        <!-- // header -->
-        <!-- container -->
-        <div id="container">
-            <div id="lnb" v-if="config.layout">
-              <Lnb :menu="menuInfo" />
-            </div>
-            <div id="contents">
-              <router-view />
-            </div>
-        </div>
-        <!-- // container -->
-        <div id="footer"></div>
-    </div>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
 </template>
+
 <script>
-import Gnb from '@/components/layout/Gnb'
-import Lnb from '@/components/layout/Lnb'
-import { setGlbConfig } from '@/components/module/common.js'
+import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'App',
+  name: 'app',
   components: {
-    Gnb,
-    Lnb
-  },
-  methods: {
-  },
-  data () {
-    return {
-      config: {
-        layout: true
-      },
-      // menuInfo: {
-      //   menuList: menuList, // 메뉴 리스트
-      // }
-    }
-  },
-  created () {
-  },
-  mounted () {
-    const vm = this
-    if (location.pathname === '/html/publishing' || location.pathname === '/login') {
-      this.config.layout = false
-      document.querySelector('#contents').setAttribute('class', 'no-layout')
-    } else {
-      this.config.layout = true
-    }
+    HelloWorld
   }
 }
 </script>
 
 <style>
-@import url('../static/css/reset.css');
-@import url('../static/css/layout.css');
-@import url('../static/css/element-ui.css');
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
 </style>
--- vue/elguide/src/main.js
+++ vue/elguide/src/main.js
@@ -1,29 +1,8 @@
-import Vue from 'vue';
-import App from './App';
-import router from './router';
-import ElementUI from 'element-ui';
-import locale from 'element-ui/lib/locale/lang/ko';
-// import axios from 'axios';
-// import VueAxios from 'vue-axios';
-// import moment from 'moment';
-import VueLodash from 'vue-lodash';
-import Chartkick from 'vue-chartkick';
-import Chart from 'chart.js';
-// import VueCookies from 'vue-cookies';
-import tinymce from 'vue-tinymce-editor';
+import Vue from 'vue'
+import App from './App.vue'
 
-Vue.component('tinymce', tinymce);
-Vue.use(Chartkick.use(Chart));
-Vue.use(VueLodash);
-
-Vue.use(ElementUI, { locale });
-Vue.use(VueRouter);
-
-Vue.config.productionTip = false;
+Vue.config.productionTip = false
 
 new Vue({
-    el: '#app',
-    router,
-    components: { App },
-    template: '<App/>'
-})
+  render: h => h(App),
+}).$mount('#app')
